6cd1f82d331698666856539dc9da79b8bdd3fd4c
Daniel Fadamitan <b.fadamitan2019@gmail.com>
2024-03-25 01:24:47 +0100

    👌: changed weblink

--- config/WebLink.js
+++ config/WebLink.js
@@ -1,4 +1,4 @@
-const production_url = "https://tackyfruitsapi-production.up.railway.app/";
+const production_url = "https://tacky-fruits-api.onrender.com/";
 const development_url = "http://localhost:7500";
 const MAIN_URL =
   !process.env.NODE_ENV || process.env.NODE_ENV === "development"
